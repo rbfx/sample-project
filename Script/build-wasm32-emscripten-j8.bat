0be60b2cb66a7ce45829ff28a8b2278ca95a186a
Gleb <gleb@gleblebedev.com>
2023-02-14 21:27:37 +0000

    Consistent folder names

--- Script/build-wasm32-emscripten-j8.bat
+++ Script/build-wasm32-emscripten-j8.bat
@@ -1,5 +1,6 @@
-if not exist %~dp0\..\cmake-build-web\* mkdir %~dp0\..\cmake-build-web
-pushd %~dp0\..\cmake-build-web
+set cmakefolder = %~dp0\..\cmake-build-wasm32-emscripten
+if not exist %cmakefolder%\* mkdir %cmakefolder%
+pushd %cmakefolder%
 del bin\*.pak
 cmake --build . -j8
 popd
--- Script/configure-x64-windows-vs2022.bat
+++ Script/configure-x64-windows-vs2022.bat
@@ -1,4 +1,5 @@
-if not exist %~dp0\..\cmake-build-win64\* mkdir %~dp0\..\cmake-build-win64
-pushd %~dp0\..\cmake-build-win64
+set cmakefolder=%~dp0\..\cmake-build-x64-windows
+if not exist %cmakefolder%\* mkdir %cmakefolder%
+pushd %cmakefolder%
 cmake -G "Visual Studio 17 2022" -DCMAKE_INSTALL_PREFIX=SDK -DCMAKE_BUILD_TYPE=RelWithDebInfo -DURHO3D_PACKAGING=ON -DBUILD_SHARED_LIBS=OFF -S ./.. -B .
 popd
