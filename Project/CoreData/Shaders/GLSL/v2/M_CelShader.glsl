f2ddcbbdc15b4eebaf6e3eacfc42837bf1d32ee0
Eugene Kozlov <nekoeuge@gmail.com>
2023-11-17 14:15:48 +0400

    Update CoreData.

--- Project/CoreData/Shaders/GLSL/v2/M_CelShader.glsl
+++ Project/CoreData/Shaders/GLSL/v2/M_CelShader.glsl
@@ -12,46 +12,29 @@
 
 UNIFORM_BUFFER_BEGIN(4, Material)
     DEFAULT_MATERIAL_UNIFORMS
-    UNIFORM(float cWindHeightFactor)
-    UNIFORM(float cWindHeightPivot)
-    UNIFORM(float cWindPeriod)
-    UNIFORM(vec2 cWindWorldSpacing)
-#ifdef WINDSTEMAXIS
-    UNIFORM(vec3 cWindStemAxis)
-#endif
+    UNIFORM(half3 cStepOffsetWidthBrightness)
 UNIFORM_BUFFER_END(4, Material)
 
-
 #include "_Material.glsl"
-
-VERTEX_OUTPUT_HIGHP(vec2 vDetailTexCoord)
 
 #ifdef URHO3D_VERTEX_SHADER
 void main()
 {
     VertexTransform vertexTransform = GetVertexTransform();
-
-    vec3 worldPos = vertexTransform.position.xyz;
-    #ifdef WINDSTEMAXIS
-        float stemDistance = dot(iPos.xyz, cWindStemAxis);
-    #else
-        float stemDistance = iPos.y;
-    #endif
-    float windStrength = max(stemDistance - cWindHeightPivot, 0.0) * cWindHeightFactor;
-    float windPeriod = cElapsedTime * cWindPeriod + dot(worldPos.xz, cWindWorldSpacing);
-    worldPos.x += windStrength * sin(windPeriod);
-    worldPos.z -= windStrength * cos(windPeriod);
-    vertexTransform.position.xyz = worldPos;
-
     Vertex_SetAll(vertexTransform, cNormalScale, cUOffset, cVOffset, cLMOffset);
-    
-    #ifdef URHO3D_XR
-        vInstID = gl_InstanceID;
-    #endif
 }
 #endif
 
 #ifdef URHO3D_PIXEL_SHADER
+
+// Based on https://github.com/glslify/glsl-aastep
+// Under MIT License
+float AntiAliasedStep(float threshold, float value)
+{
+    float afwidth = length(vec2(dFdx(value), dFdy(value))) * 0.7071068;
+    return smoothstep(threshold - afwidth, threshold + afwidth, value);
+}
+
 void main()
 {
 #ifdef URHO3D_DEPTH_ONLY_PASS
@@ -73,8 +56,35 @@ void main()
     Surface_SetEmission(surfaceData, cMatEmissiveColor, sEmission, vTexCoord, URHO3D_MATERIAL_EMISSION);
     Surface_ApplySoftFadeOut(surfaceData, vWorldDepth, cFadeOffsetScale);
 
-    half3 surfaceColor = GetSurfaceColor(surfaceData);
+#ifdef URHO3D_AMBIENT_PASS
+    half3 surfaceColor = CalculateAmbientLighting(surfaceData);
+#else
+    half3 surfaceColor = vec3(0.0);
+#endif
+
+#ifdef URHO3D_GBUFFER_PASS
+    gl_FragData[1] = vec4(surfaceData.fogFactor * surfaceData.albedo.rgb, 0.0);
+    gl_FragData[2] = vec4(surfaceData.fogFactor * surfaceData.specular, surfaceData.roughness);
+    gl_FragData[3] = vec4(surfaceData.normal * 0.5 + 0.5, 0.0);
+#elif defined(URHO3D_LIGHT_PASS)
+    DirectLightData lightData = GetForwardDirectLightData();
+    float NoL = dot(surfaceData.normal, lightData.lightVec.xyz);
+    float shadowStep = AntiAliasedStep(cStepOffsetWidthBrightness.x-cStepOffsetWidthBrightness.y*0.5, NoL) * cStepOffsetWidthBrightness.z;
+    float lightStep = AntiAliasedStep(cStepOffsetWidthBrightness.x+cStepOffsetWidthBrightness.y*0.5, NoL);
+    float steppedNoL = mix(shadowStep, 1.0, lightStep);
+ #if (URHO3D_SPECULAR > 0)
+    half3 halfVec = normalize(surfaceData.eyeVec + lightData.lightVec.xyz);
+    float brdf = BRDF_Direct_BlinnPhongSpecular(surfaceData.normal, halfVec, cMatSpecColor.a);
+    half3 specular = max(NoL, 0.0) * surfaceData.specular * (brdf * cLightColor.a);
+#else
+    half3 specular = half3(0.0, 0.0, 0.0);
+#endif
+    surfaceColor += lightData.lightColor * (steppedNoL * surfaceData.albedo.rgb + specular) * GetDirectLightAttenuation(lightData);
+
+#endif
+
     gl_FragColor = GetFragmentColorAlpha(surfaceColor, surfaceData.albedo.a, surfaceData.fogFactor);
 #endif
+
 }
 #endif
