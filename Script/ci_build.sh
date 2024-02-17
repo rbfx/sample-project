5abb2a5b0fce0da69b57786f32f44862465a8776
Eugene Kozlov <nekoeuge@gmail.com>
2024-02-17 12:44:28 +0400

    Add subdirectory build to CI.

--- Script/ci_build.sh
+++ Script/ci_build.sh
@@ -6,6 +6,7 @@
 # ci_build_dir:      cmake cache directory
 # ci_native_sdk_dir: native SDK (linux) installation directory
 # ci_target_sdk_dir: target SDK (web) installation directory
+# ci_build_mode:     sdk|subdirectory
 
 ci_action=$1; shift;
 ci_source_dir=${ci_source_dir%/};   # remove trailing slash if any
@@ -15,12 +16,16 @@ echo "ci_source_dir=$ci_source_dir"
 echo "ci_build_dir=$ci_build_dir"
 echo "ci_native_sdk_dir=$ci_native_sdk_dir"
 echo "ci_target_sdk_dir=$ci_target_sdk_dir"
+echo "ci_build_mode=$ci_build_mode"
 
 function action-dependencies() {
     sudo apt-get install -y --no-install-recommends uuid-dev ninja-build
 }
 
 function action-generate() {
+    local extra_params=""
+    if [[ $ci_build_mode == "sdk" ]]; then extra_params="-DREBELFORK_SDK=$ci_target_sdk_dir"; fi
+
     cmake \
         -G Ninja                                                                                    \
         -DCMAKE_TOOLCHAIN_FILE=$EMSDK/upstream/emscripten/cmake/Modules/Platform/Emscripten.cmake   \
@@ -28,10 +33,10 @@ function action-generate() {
         -DEMSCRIPTEN_ROOT_PATH=$EMSDK/upstream/emscripten/                                          \
         -DCMAKE_BUILD_TYPE=Release                                                                  \
         -DURHO3D_SDK=$ci_native_sdk_dir                                                             \
-        -DREBELFORK_SDK=$ci_target_sdk_dir                                                          \
         -DCI_WEB_BUILD=ON                                                                           \
         -DBUILD_SHARED_LIBS=OFF                                                                     \
         -DURHO3D_PROFILING=OFF                                                                      \
+        $extra_params                                                                               \
         -B $ci_build_dir -S "$ci_source_dir"
 }
 
