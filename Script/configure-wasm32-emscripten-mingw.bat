8e37032e0b53191233e860a0855f0d8ba34919c7
TEDERIs <xcplay@gmail.com>
2025-05-05 13:18:31 +0700

    Allow the use of paths with spaces

--- Script/configure-wasm32-emscripten-mingw.bat
+++ Script/configure-wasm32-emscripten-mingw.bat
@@ -1,6 +1,6 @@
 set cmakefolder=%~dp0\..\cmake-build-wasm32-emscripten
-if not exist %cmakefolder%\* mkdir %cmakefolder%
-pushd %cmakefolder%
+if not exist "%cmakefolder%\*" mkdir "%cmakefolder%"
+pushd "%cmakefolder%"
 rem EMSCRIPTEN_ROOT_PATH=path/to/emsdk/upstream/emscripten
 cmake -G "MinGW Makefiles" -DCMAKE_TOOLCHAIN_FILE=%EMSCRIPTEN_ROOT_PATH%/cmake/Modules/Platform/Emscripten.cmake -DCMAKE_INSTALL_PREFIX=SDK -DCMAKE_BUILD_TYPE=RelWithDebInfo -DURHO3D_PACKAGING=ON -DBUILD_SHARED_LIBS=OFF -DEMSCRIPTEN_ROOT_PATH=%EMSCRIPTEN_ROOT_PATH% -S ../ -B .
 popd
--- Script/configure-x64-uwp-vs2022.bat
+++ Script/configure-x64-uwp-vs2022.bat
@@ -1,5 +1,5 @@
 set cmakefolder=%~dp0\..\cmake-build-x64-uwp
-if not exist %cmakefolder%\* mkdir %cmakefolder%
-pushd %cmakefolder%
+if not exist "%cmakefolder%\*" mkdir "%cmakefolder%"
+pushd "%cmakefolder%"
 cmake -G "Visual Studio 17 2022" -A x64 -DCMAKE_SYSTEM_NAME=WindowsStore -DCMAKE_SYSTEM_VERSION='10.0' -DCMAKE_VS_WINDOWS_TARGET_PLATFORM_VERSION='10.0.17763.0' -DCMAKE_INSTALL_PREFIX=SDK -DCMAKE_BUILD_TYPE=RelWithDebInfo -DURHO3D_PACKAGING=ON -DBUILD_SHARED_LIBS=OFF -S ./.. -B .
 popd
